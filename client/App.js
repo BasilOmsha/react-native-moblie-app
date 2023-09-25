de90815d8203030a4f8f000feb0026e5e12ca0e7
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-09-25 18:06:06 +0300

    Signup stack navigation moved to its own file.

--- client/App.js
+++ client/App.js
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet, Image, FlatList, TouchableOpacity, TextInput } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import RegistrationScreen from './src/components/Register/RegistrationScreen';
-import LoginScreen from './src/components/Login/LoginScreen';
-import { User } from './src/services/UserContext';
-import { Labels } from './src/services/signupServices/SignupLabelsContext';
+import React from 'react';
+// import { View, Text, Button, StyleSheet, Image, FlatList, TouchableOpacity, TextInput } from 'react-native';
+import SignupNav from './src/services/signupServices/SignupNav';
 import styles from './src/styles/Style';
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
 
   return (
-    <User>
-      <Labels>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Registration" component={RegistrationScreen} options={{ headerStyle: { backgroundColor: '#27aae2' }, headerTintColor: 'white' }} />
-            <Stack.Screen name="Login" component={LoginScreen} options={{ headerStyle: { backgroundColor: '#27aae2' }, headerTintColor: 'white' }} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Labels>
-    </User>
+    <SignupNav />
   );
 };
 export default App;
---
+++ client/src/services/signupServices/SignupNav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import { View, Text, Button, StyleSheet, Image, FlatList, TouchableOpacity, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RegistrationScreen from '../../components/Register/RegistrationScreen';
+import LoginScreen from '../../components/Login/LoginScreen';
+import { User } from '../UserContext';
+import { Labels } from './SignupLabelsContext';
+import styles from '../../styles/Style';
+
+const Stack = createNativeStackNavigator();
+
+const SignupNav = () => {
+
+  return (
+    <User>
+      <Labels>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Registration" component={RegistrationScreen} options={{ headerStyle: { backgroundColor: '#27aae2' }, headerTintColor: 'white' }} />
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerStyle: { backgroundColor: '#27aae2' }, headerTintColor: 'white' }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Labels>
+    </User>
+  );
+};
+export default SignupNav;
