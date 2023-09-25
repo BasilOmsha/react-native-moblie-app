48355d8c519ff3f7dafac88bdf241d27ef3ccc91
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-09-25 03:18:32 +0300

    home page, profile page created

---
+++ client/components/home/HomePage.js
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import {styles} from '../../styles/globalStyles';
+const HomePage = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to home page</Text>
+    </View>
+  );
+};
+export default HomePage;
---
+++ client/components/profile/UserProfile.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+import {styles} from '../../styles/globalStyles';
+
+const UserProfile = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to profile page</Text>
+    </View>
+  );
+};
+export default UserProfile;
