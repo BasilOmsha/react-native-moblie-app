7b7447c4f587c39d7dc84366ae66e9421f956ede
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-09-29 14:54:36 +0300

    Login front end built.

--- client/src/components/Login/LoginScreen.js
+++ client/src/components/Login/LoginScreen.js
@@ -3,23 +3,86 @@ import {
     StyleSheet, TextInput, View, Text, Image, KeyboardAvoidingView, Keyboard, TouchableOpacity, ScrollView,
 } from 'react-native';
 import { useUserContext } from '../../services/UserContext';
-import { useSignupFormContext } from '../components/signupServices/SignupLabelsContext';
+import { useSignupFormContext } from '../../services/signupServices/SignupLabelsContext';
 import Loader from '../../services/Loader';
-import styles from '../../styles/SignupStyle';
+import styles from '../../styles/LoginStyle';
 
 const LoginScreen = ({ navigation }) => {
+    const userContext = useUserContext();
+
+    const labelContext = useSignupFormContext();
     return (
-        <View>
-            <Text>Hellow World!</Text>
-            < TouchableOpacity
-                style={styles.buttonStyle2}
-                activeOpacity={0.5}
-                onPress={() => {
-                    // change to login screeb
-                    navigation.navigate('Registration | Basic Info');
-                }} >
-                <Text style={styles.buttonTextStyle}>Register</Text>
-            </TouchableOpacity >
+        <View style={styles.mainLoginContainer}>
+            <Loader loading={labelContext.loading} />
+            <ScrollView keyboardShouldPersistTaps="handled">
+                <View style={{ alignItems: 'center' }}>
+                    <Image
+                        source={require('../../assets/images/airplain.png')}
+                        style={{
+                            width: '50%',
+                            height: 80,
+                            resizeMode: 'contain',
+                            margin: 20,
+                        }}
+                    />
+                </View>
+                <View style={styles.SectionStyle}>
+                    {labelContext.renderLablEmail()}
+                    <TextInput
+                        style={[styles.inputStyle, labelContext.isFocusEmail && { borderColor: 'blue' }]}
+                        underlineColorAndroid="#f000"
+                        placeholder={!labelContext.isFocusEmail ? 'Email' : 'Email'}
+                        placeholderTextColor="#777777"
+                        onFocus={async () => {
+                            labelContext.setIsFocusEmail(true)
+                        }}
+                        onBlur={async () => { labelContext.setIsFocusEmail(false) }}
+                        onChange={async (item) => {
+                            // setValue(item.value);
+                            labelContext.setIsFocusEmail(true);
+                        }}
+                        onChangeText={userContext.emailInputHandler}
+                        value={userContext.email}
+                    />
+                </View>
+                <View style={styles.SectionStyle}>
+                    {labelContext.renderLablPaswd()}
+                    <TextInput
+                        style={[styles.inputStyle, labelContext.isFocusPaswd && { borderColor: 'blue' }]}
+                        underlineColorAndroid="#f000"
+                        placeholder={!labelContext.isFocusPaswd ? 'Password' : 'Password'}
+                        placeholderTextColor="#777777"
+                        onFocus={async () => {
+                            labelContext.setIsFocusPaswd(true)
+                        }}
+                        onBlur={async () => { labelContext.setIsFocusPaswd(false) }}
+                        onChange={async (item) => {
+                            // setValue(item.value);
+                            labelContext.setIsFocusPaswd(true);
+                        }}
+                        onChangeText={userContext.passwordInputHandler}
+                        value={userContext.password}
+                    />
+                </View>
+                < TouchableOpacity
+                    style={styles.loginButton}
+                    activeOpacity={0.5}
+                    onPress={() => {
+                        // navigation.navigate('Registration | Basic Info');
+                    }} >
+                    <Text style={styles.loginbuttonTextStyle}>Login</Text>
+                </TouchableOpacity >
+                <View style={styles.signupLinkContainer}>
+                    <Text style={styles.TextStyle}> Don't Have an account?</Text>
+                    < TouchableOpacity
+                        style={styles.signuplink}
+                        onPress={() => {
+                            navigation.navigate('Registration | Basic Info');
+                        }} >
+                        <Text style={styles.signupLinkTextStyle}>   Register</Text>
+                    </TouchableOpacity >
+                </View>
+            </ScrollView >
         </View>
     )
 }
---
+++ client/src/styles/LoginStyle.js
@@ -0,0 +1,77 @@
+// Signup form styles
+import { StyleSheet } from 'react-native';
+
+const loginStyle = StyleSheet.create({
+    mainLoginContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignContent: 'center'
+    },
+
+    SectionStyle: {
+        flexDirection: 'row',
+        height: 45,
+        marginTop: 20,
+        marginLeft: 35,
+        marginRight: 35,
+        margin: 10,
+    },
+
+    inputStyle: {
+        flex: 1,
+        color: '#000',
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderWidth: 1,
+        borderRadius: 8,
+        borderColor: '#000',
+    },
+
+    loginButton: {
+        backgroundColor: '#27aae2',
+        borderWidth: 0,
+        color: '#fff',
+        borderColor: '#27aae2',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        borderRadius: 8,
+        marginLeft: 105,
+        marginRight: 95,
+        marginTop: 5,
+        marginBottom: 5,
+    },
+
+    loginbuttonTextStyle: {
+        color: '#fff',
+        paddingVertical: 10,
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    signupLinkContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 80,
+        marginLeft: 95,
+    },
+    signuplink: {
+        borderWidth: 0,
+        height: 40,
+        width: 200,
+        borderRadius: 8,
+    },
+
+    TextStyle: {
+        paddingVertical: 10,
+        fontWeight: 'bold'
+    },
+    signupLinkTextStyle: {
+        color: '#093573',
+        paddingVertical: 10,
+        fontWeight: 'bold'
+    },
+
+});
+
+export default loginStyle;
