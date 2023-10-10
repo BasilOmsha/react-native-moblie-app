e3afc5118bc2dac4391b3906e82518568e8bd628
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-10-10 18:49:45 +0300

    added the extra components

--- client/components/homePage/HomePage.js
+++ client/components/homePage/HomePage.js
@@ -1,26 +1,111 @@
 import React, {useState, useEffect} from 'react';
-import {Button, View, TextInput, ActivityIndicator, Text} from 'react-native';
+import {
+  Button,
+  View,
+  TextInput,
+  ActivityIndicator,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Image,
+} from 'react-native';
 import {fetchLocations} from '../../services/searchServices';
+import {airlines, flightDurations24H, stops} from '../../data/data';
+import {capitalizeFirstLetter, formatDate} from '../../helper/helper';
+import {styles} from '../../styles/globalStyles';
+import CalendarPicker from 'react-native-calendar-picker';
+import {ModalScrollView} from '../flatList/modalScrollView';
+import {useData} from '../../context/globalData';
 
 function HomePage({navigation}) {
-  const [from, setFrom] = useState('Denmark');
-  const [to, setTo] = useState('Spain');
-  const [date, setDate] = useState('2023-11-11');
+  const [from, setFrom] = useState('Copenhagen');
+  const [to, setTo] = useState('Helsinki');
+  const [date, setDate] = useState(new Date());
   const [round, setRound] = useState(false);
-  const [returnDate, setReturnDate] = useState('2023-02-01');
+  const [returnDate, setReturnDate] = useState();
   const [loading, setLoading] = useState(false);
-
+  const [selectedAirline, setSelectedAirline] = useState('');
+  const [selectedStops, setSelectedStops] = useState('');
+  const [selectedMaxDuration, setSelectedMaxDuration] = useState('');
+  const [isOneWayPressed, setIsOneWayPressed] = useState(false);
+  const [isReturnPressed, setIsReturnPressed] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalReturnVisible, setModalReturnVisible] = useState(false);
+  const [isSelectedAirline, setIsSelectedAirline] = useState('');
+  const [isSelectedDuration, setIsSelectedDuration] = useState('');
+  const [isDurationModalVisible, setIsDurationModalVisible] = useState(false);
+  const [isStopsModalVisible, setIsStopsModalVisible] = useState(false);
+  const [isModalLoading, setIsModalLoading] = useState('');
+  const {globalData, addData} = useData();
   const handleOneWay = () => {
     setRound(false);
+    setIsOneWayPressed(true);
+    setIsReturnPressed(false);
+    setReturnDate('');
+  };
+
+  const toggleDate = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  const handleSwitch = () => {
+    setFrom(to);
+    setTo(from);
+  };
+
+  const toggleReturnDate = () => {
+    setModalReturnVisible(!modalReturnVisible);
   };
 
   const handleReturn = () => {
     setRound(true);
+    setIsOneWayPressed(false);
+    setIsReturnPressed(true);
+  };
+
+  const clearData = () => {
+    setDate('');
+    setFrom('');
+    setTo('');
+    setReturnDate('');
+    setSelectedAirline('');
+    setIsSelectedDuration('');
+    setSelectedStops('');
+    setIsOneWayPressed('');
+    setIsReturnPressed('');
+  };
+
+  const handleAirline = value => {
+    setSelectedAirline(value);
+    toggleAirlines();
+  };
+
+  const toggleAirlines = () => {
+    setIsSelectedAirline(!isSelectedAirline);
+  };
+
+  const handleDurationToggle = () => {
+    setIsDurationModalVisible(!isDurationModalVisible);
+  };
+
+  const handleDuration = value => {
+    setIsSelectedDuration(value);
+    handleDurationToggle();
+  };
+
+  const toggleStopsModal = () => {
+    setIsStopsModalVisible(!isStopsModalVisible);
+  };
+
+  const handleStops = value => {
+    setSelectedStops(value);
+    toggleStopsModal();
   };
 
   const fetchData = async () => {
     try {
-      setLoading(true); // Start loading indicator
+      setLoading(true);
+      setIsModalLoading(true);
 
       setTimeout(async () => {
         const response = await fetchLocations(
@@ -29,55 +114,288 @@ function HomePage({navigation}) {
           date,
           round,
           returnDate,
+          selectedAirline,
+          selectedMaxDuration,
+          selectedStops,
         );
 
-        // Navigate to the Flights screen after 4 seconds
-        navigation.navigate('Flights', {
-          round,
+        // Update global data using the newData function
+        addData({
           combinedFlights: response.data.returnFlights,
+
           data: response.data.outboundFlights,
         });
 
-        setLoading(false); // Stop loading indicator
-      }, 4000); // 4 seconds delay
+        navigation.navigate('Flights', {
+          round,
+          combinedFlights: response.data.returnFlights,
+          data: response.data.outboundFlights, 
+        });
+
+        setIsModalLoading(false);
+      }, 6000);
     } catch (error) {
       console.error('Error fetching locations:', error);
-      setLoading(false); // Stop loading indicator in case of an error
     }
   };
 
   return (
-    <View style={{flex: 1}}>
-      <Button title="One-way" onPress={handleOneWay} />
-      <Button title="Return" onPress={handleReturn} />
-      <TextInput
-        placeholder="From"
-        value={from}
-        onChangeText={text => setFrom(text)}
-      />
-      <TextInput
-        placeholder="To"
-        value={to}
-        onChangeText={text => setTo(text)}
-      />
-      <TextInput
-        placeholder="Date"
-        value={date}
-        onChangeText={text => setDate(text)}
-      />
-      {round && (
-        <TextInput
-          placeholder="Return Date"
-          value={returnDate}
-          onChangeText={text => setReturnDate(text)}
-        />
-      )}
-      <Button title="Search flight" onPress={fetchData} />
+    <View style={{flex: 1, backgroundColor: '#F3F3F3'}}>
       {loading && (
-        <ActivityIndicator
-          size="large"
-          style={{marginTop: 20}}></ActivityIndicator>
+        <View style={styles.circle}>
+          <Modal visible={isModalLoading}>
+            <Image
+              style={{
+                height: 800,
+                width: 400,
+              }}
+              source={require('../../assets/loading3.jpg')}
+            />
+            <Image
+              style={{
+                height: 80,
+                width: 80,
+                position: 'relative',
+                bottom: 750,
+                left: 60,
+              }}
+              source={require('../../assets/airplain.png')}
+            />
+            <ActivityIndicator
+              size={100}
+              style={{position: 'absolute', top: 400, left: 148}}
+              color="#5F6A6A"
+            />
+
+            <Text
+              style={{
+                bottom: 350,
+                color: '#CCD1D1',
+                fontSize: 18,
+                fontWeight: 'bold',
+                textAlign: 'center',
+              }}>
+              {`Searching flights to ${to} ${formatDate(date)}`}
+            </Text>
+          </Modal>
+        </View>
       )}
+      <View style={styles.HomePage}>
+        <TouchableOpacity
+          onPress={handleOneWay}
+          style={[styles.HomeButton, isOneWayPressed && styles.PressedButton]}>
+          <Text style={styles.HomePageTabsText}>One-way</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleReturn}
+          style={[styles.HomeButton, isReturnPressed && styles.PressedButton]}>
+          <Text style={styles.HomePageTabsText}>Round-trip</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          marginTop: 30,
+          marginBottom: 30,
+          marginRight: 10,
+          marginLeft: 10,
+        }}>
+        <TouchableOpacity onPress={toggleAirlines}>
+          <Text style={styles.filterHeaderA}>Airlines</Text>
+        </TouchableOpacity>
+        <Modal visible={isSelectedAirline} transparent={true}>
+          <ModalScrollView
+            data={airlines}
+            handleInput={handleAirline}
+            toggle={toggleAirlines}
+          />
+        </Modal>
+        <TouchableOpacity onPress={handleDurationToggle}>
+          <Text style={styles.filterHeaderD}>Duration</Text>
+        </TouchableOpacity>
+        <Modal visible={isDurationModalVisible} transparent={true}>
+          <ModalScrollView
+            data={flightDurations24H}
+            handleInput={handleDuration}
+            toggle={handleDurationToggle}
+          />
+        </Modal>
+        <TouchableOpacity onPress={toggleStopsModal}>
+          <Text style={styles.filterHeaderS}>Stops</Text>
+        </TouchableOpacity>
+        <Modal visible={isStopsModalVisible} transparent={true}>
+          <ModalScrollView
+            data={stops}
+            handleInput={handleStops}
+            toggle={toggleStopsModal}
+          />
+        </Modal>
+      </View>
+
+      <TextInput
+        style={styles.destinationFrom}
+        placeholder={` 🛫  From : Please enter city...`}
+        value={from}
+        onChangeText={text => setFrom(capitalizeFirstLetter(text))}
+      />
+
+      <TextInput
+        style={styles.destinationTo}
+        placeholder={` 🛬  To : Please enter city...`}
+        value={to}
+        onChangeText={text => setTo(capitalizeFirstLetter(text))}
+      />
+      <TouchableOpacity onPress={handleSwitch}>
+        <Text style={styles.switch}>🔄</Text>
+      </TouchableOpacity>
+      <View style={styles.dateInput}>
+        <TouchableOpacity onPress={toggleDate}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <TextInput
+              style={styles.textBold}
+              placeholder={`Date ${new Date().toLocaleDateString('en-US', {
+                month: 'short',
+                day: 'numeric',
+              })}`}
+              value={date}
+              editable={false}
+            />
+            <Text style={{fontSize: 22}}>🗓️</Text>
+          </View>
+        </TouchableOpacity>
+
+        <Modal visible={modalVisible} animationType="slide" transparent={true}>
+          <View style={styles.modalPage}>
+            <CalendarPicker
+              minDate={new Date()}
+              selectedStartDate={date}
+              onDateChange={selectedDate => {
+                const formattedDate = selectedDate.toISOString().split('T')[0];
+                setDate(formattedDate);
+                toggleDate();
+              }}
+              width={400}
+              height={400}
+              textStyle={{color: 'black', fontWeight: 'bold'}}
+              selectedDayColor="blue"
+              selectedDayTextColor="white"
+              todayBackgroundColor="lightgray"
+              todayTextStyle={{fontWeight: 'bold'}}
+            />
+            <Button title="cancel" onPress={toggleDate}></Button>
+          </View>
+        </Modal>
+
+        {round && (
+          <TouchableOpacity onPress={toggleReturnDate}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <TextInput
+                style={styles.textBold}
+                placeholder={`Return ${new Date().toLocaleDateString('en-US', {
+                  month: 'short',
+                  day: 'numeric',
+                })}`}
+                value={returnDate}
+                editable={false}
+              />
+              <Text style={{fontSize: 22}}>🗓️</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+        <Modal
+          visible={modalReturnVisible}
+          animationType="slide"
+          transparent={true}>
+          <View style={styles.modalPage}>
+            <CalendarPicker
+              minDate={new Date()}
+              selectedStartDate={returnDate}
+              onDateChange={selectedDate => {
+                const formattedDate = selectedDate.toISOString().split('T')[0];
+                setReturnDate(formattedDate);
+                toggleReturnDate();
+              }}
+              width={400}
+              height={400}
+              textStyle={{color: 'black', fontWeight: 'bold'}}
+              selectedDayColor="blue"
+              selectedDayTextColor="white"
+              todayBackgroundColor="lightgray"
+              todayTextStyle={{fontWeight: 'bold'}}
+            />
+            <Button title="cancel" onPress={toggleReturnDate}></Button>
+          </View>
+        </Modal>
+      </View>
+      {from && (
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: 'white',
+            marginLeft: 10,
+            marginRight: 10,
+            padding: 10,
+            borderRadius: 8,
+          }}>
+          <View style={{backgroundColor: 'red', width: 4}}>
+            <Text></Text>
+          </View>
+
+          <View>
+            {from && (
+              <Text style={from && styles.filterResults}>From: {from}</Text>
+            )}
+            {to && <Text style={to && styles.filterResults}>To: {to}</Text>}
+            {date && (
+              <Text style={date && styles.filterResults}>
+                Date: {formatDate(date)}
+              </Text>
+            )}
+            {returnDate && (
+              <Text style={returnDate && styles.filterResults}>
+                Return: {formatDate(returnDate)}
+              </Text>
+            )}
+            {isOneWayPressed && (
+              <Text style={isOneWayPressed && styles.filterResults}>
+                Trip: {`One-way`}
+              </Text>
+            )}
+            {isReturnPressed && (
+              <Text style={isReturnPressed && styles.filterResults}>
+                Trip: {`Round-trip`}
+              </Text>
+            )}
+          </View>
+          <View style={{marginRight: 10}}>
+            {selectedStops && (
+              <Text style={selectedStops && styles.filterResults}>
+                Stops: {selectedStops}
+              </Text>
+            )}
+            {isSelectedDuration && (
+              <Text style={isSelectedDuration && styles.filterResults}>
+                Duration: {isSelectedDuration}
+              </Text>
+            )}
+            {selectedAirline && (
+              <Text style={selectedAirline && styles.filterResults}>
+                Airlines: {selectedAirline}
+              </Text>
+            )}
+          </View>
+        </View>
+      )}
+
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        <TouchableOpacity onPress={fetchData}>
+          <Text style={styles.buttonsF}>Search flight</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={clearData}>
+          <Text style={styles.buttonsS}>Clear data</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
