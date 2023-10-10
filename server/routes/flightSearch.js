658b1564cf550a503b2082438e344176f6aba3f9
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-10-11 00:57:57 +0300

    Server merged

--- server/routes/flightSearch.js
+++ server/routes/flightSearch.js
@@ -1,45 +1,114 @@
-// routes/flightSearch.js
 const express = require("express");
 const router = express.Router();
 const { generateRandomFlight } = require("../utils/generateRandomFlight");
 const { europeanCountriesWithAirports } = require("../data");
 
 router.get("/", (req, res) => {
-  const { from, to, date } = req.query;
+  const {
+    from,
+    to,
+    date,
+    round,
+    returnDate,
+    selectedAirline,
+    selectedMaxDuration,
+    selectedStops,
+  } = req.query;
 
+  // Find the country objects for both 'from' and 'to' based on the country names
   const countryFrom = europeanCountriesWithAirports.find(
-    (entry) => entry.country === from
+    (entry) => entry.capitalCity === from
   );
   const countryTo = europeanCountriesWithAirports.find(
-    (entry) => entry.country === to
+    (entry) => entry.capitalCity === to
   );
 
   if (!countryFrom || !countryTo) {
     return res.status(400).json({ error: "Invalid country name" });
   }
 
+  // Now, you can access the capital cities as needed
+  const capitalCityFrom = countryFrom.country;
+  const capitalCityTo = countryTo.country;
+
   const startDate = new Date(date);
   if (isNaN(startDate.getTime())) {
     return res.status(400).json({ error: "Invalid date format" });
   }
 
-  const numberOfFlights = 15;
+  const numberOfFlights = 5;
 
-  const flights = Array.from({ length: numberOfFlights }, (_, index) => {
-    // Calculate the date for each flight based on the index
-    const flightStartDate = new Date(startDate);
-    flightStartDate.setDate(flightStartDate.getDate() + index);
+  const outboundFlights = Array.from(
+    { length: numberOfFlights },
+    (_, index) => {
+      // Calculate the date for each outbound flight based on the index
+      const outboundStartDate = new Date(startDate);
+      outboundStartDate.setDate(outboundStartDate.getDate() + index);
 
-    return generateRandomFlight(
-      countryFrom.airport,
-      countryTo.airport,
-      flightStartDate,
-      from,
-      to
-    );
+      const filteredFlights = generateRandomFlight(
+        countryFrom.airport,
+        countryTo.airport,
+        outboundStartDate,
+        from,
+        to,
+        selectedAirline,
+        selectedMaxDuration,
+        selectedStops,
+        capitalCityFrom,
+        capitalCityTo
+      );
+
+      return filteredFlights; // Return filtered flights
+    }
+  );
+
+  // If round trip is selected, generate return flights as well
+  let returnFlights = [];
+  if (round && returnDate && startDate) {
+    const returnStartDate = new Date(returnDate);
+
+    returnFlights = Array.from({ length: numberOfFlights }, (_, index) => {
+      // Calculate the date for each return flight based on the index and return date
+      const returnFlightStartDate = new Date(returnStartDate);
+      returnFlightStartDate.setDate(returnFlightStartDate.getDate() + index);
+      const outboundStartDate = new Date(startDate);
+      outboundStartDate.setDate(outboundStartDate.getDate() + index);
+      const outboundFlight = generateRandomFlight(
+        countryFrom.airport,
+        countryTo.airport,
+        outboundStartDate,
+        from,
+        to,
+        selectedAirline,
+        selectedMaxDuration,
+        selectedStops,
+        capitalCityFrom,
+        capitalCityTo
+      );
+
+      const returnFlight = generateRandomFlight(
+        countryTo.airport,
+        countryFrom.airport,
+        returnFlightStartDate,
+        to,
+        from,
+        selectedAirline,
+        selectedMaxDuration,
+        selectedStops,
+        capitalCityTo,
+        capitalCityFrom
+      );
+
+      return { outbound: outboundFlight[index], return: returnFlight[index] };
+    });
+  }
+
+  res.json({
+    outboundFlights,
+    returnFlights,
+    capitalCityFrom, // Include capital cities in the response if needed
+    capitalCityTo,
   });
-
-  res.json({ flights });
 });
 
 module.exports = router;
--- server/utils/generateRandomFlight.js
+++ server/utils/generateRandomFlight.js
@@ -1,15 +1,43 @@
-// utils/generateRandomFlight.js
 const { europeanCountriesWithAirports, airlines } = require("../data");
 
-function generateRandomFlight(from, to, startDate, fromCountry, toCountry) {
+function generateRandomFlight(
+  from,
+  to,
+  startDate,
+  fromCountry,
+  toCountry,
+  selectedAirline,
+  selectedMaxDuration,
+  selectedStops,
+  fromCapitalCity,
+  capitalCityTo
+) {
   const flights = []; // Create an array to store flight objects
-  const numberOfFlights = 3; // Number of flights for the same date
+  const numberOfFlights = 10; // Number of flights for the same date
 
   for (let i = 0; i < numberOfFlights; i++) {
     const isDirectFlight = Math.random() < 0.5;
-    const durationHours = isDirectFlight
-      ? 4
-      : Math.floor(Math.random() * 10) + 1;
+
+    // Check selectedStops filter
+    if (
+      (selectedStops === "Direct" && !isDirectFlight) ||
+      (selectedStops === "One Stop" && isDirectFlight)
+    ) {
+      continue; // Skip the flight if it doesn't match the selectedStops filter
+    }
+
+    // Generate a random duration based on the user's input range
+    let durationHours;
+    if (selectedMaxDuration && selectedMaxDuration !== "Any Duration") {
+      const [minHours, maxHours] = selectedMaxDuration
+        .split("h")
+        .map((str) => parseInt(str));
+      durationHours =
+        Math.floor(Math.random() * (maxHours - minHours + 1)) + minHours;
+    } else {
+      durationHours = isDirectFlight ? 4 : Math.floor(Math.random() * 10) + 1;
+    }
+
     const durationMinutes = isDirectFlight ? 0 : Math.floor(Math.random() * 60);
 
     // Generate a random departure time within the specified date
@@ -27,21 +55,36 @@ function generateRandomFlight(from, to, startDate, fromCountry, toCountry) {
       : europeanCountriesWithAirports[
           Math.floor(Math.random() * europeanCountriesWithAirports.length)
         ].country;
-    const airline = airlines[Math.floor(Math.random() * airlines.length)];
-    const durationString = `${durationHours}h ${durationMinutes}m`;
 
-    // Generate a random price between 100 and 1000
+    let airline;
+
+    if (!selectedAirline || selectedAirline === "Airlines") {
+      // If no airline filter is specified or "All Airlines" is selected, choose a random airline
+      airline = airlines[Math.floor(Math.random() * airlines.length)];
+    } else {
+      // If a specific airline is selected, use it
+      airline = selectedAirline;
+    }
+
+    // Calculate the duration string
+    const duration = `${durationHours}h ${durationMinutes}m`;
+
+    // Generate a random price within the specified range
     const price = Math.floor(Math.random() * 901) + 100;
 
     const flight = {
-      from: { country: fromCountry, airport: from },
-      to: { country: toCountry, airport: to },
+      from: {
+        country: fromCountry,
+        airport: from,
+        capitalCity: fromCapitalCity,
+      },
+      to: { country: toCountry, airport: to, capitalCity: capitalCityTo },
       departureTime,
       arrivalTime,
       isDirectFlight,
       stopoverCountry,
-      airline,
-      duration: durationString,
+      airline, // Assign the selected or random airline name
+      duration,
       price,
     };
 
