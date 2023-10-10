f5a364b896790a2e1d786c9dbd0cde01fb010196
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-10-10 18:46:02 +0300

    added stack navigation to main app tab nav

--- client/components/navigation/mainAppNav.js
+++ client/components/navigation/mainAppNav.js
@@ -1,17 +1,32 @@
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 import UserProfile from '../profilePage/UserProfile';
 import HomePage from '../homePage/HomePage';
 import FlightSearchResults from '../flightSearchResults/flightSearchResults';
+import FlightHeader from '../../flightSearchHeader/flightHeaderDate';
+import FlightDetails from '../flightDetails/flightDetails';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const HomeStackScreen = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#27aae2', // Set your desired background color
+      },
+    }}>
     <Stack.Screen name="Home" component={HomePage} />
-    <Stack.Screen name="Flights" component={FlightSearchResults} />
+    <Stack.Screen
+      name="Flights"
+      options={{
+        headerRight: () => <FlightHeader />,
+      }}
+      component={FlightSearchResults}
+    />
+    <Stack.Screen name="FlightDetails" component={FlightDetails} />
   </Stack.Navigator>
 );
 
