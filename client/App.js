ff45d516f4e49c679e8bba301b24513768011ee2
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-10-10 18:41:01 +0300

    wraped the app with global variable provider

--- client/App.js
+++ client/App.js
@@ -1,8 +1,9 @@
 import NavButton from './components/navigation/mainAppNav';
+import {DataProvider} from './context/globalData';
 
 const App = () => (
-  <>
+  <DataProvider>
     <NavButton />
-  </>
+  </DataProvider>
 );
 export default App;
